8c98156cd63ead1e9db1e47f034a2bd39d7cc416
Meathill <meathill@gmail.com>
2023-06-12 00:25:41 +0800

    change: daily work

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,18 +5,7 @@ const runtimeConfig = useRuntimeConfig();
 
 <template lang="pug">
 footer.footer.p-4.container.mx-auto.border-t(class="sm:mt-auto")
-  .w-56
-    h1.text-xl.font-bold Abalone.ai
-    p.text-gray-500.text-base Generate outside the box thinking with the possibility to targtet the low.
-    .w-full.flex.text-2xl.gap-6
-      a
-        i.bi.bi-facebook
-      a
-        i.bi.bi-twitter
-      a
-        i.bi.bi-youtube
-
-p.py-4.text-center.text-xs.font-light.text-gray-400 Copyright © {{year}} - All right reserved - v{{runtimeConfig.public.version}}
+  p.py-4.text-center.text-sm.font-light.text-gray-400 Copyright © {{year}} - All right reserved - v{{runtimeConfig.public.version}}
 </template>
 
 <script lang="ts">
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,74 @@
 </script>
 
 <template lang="pug">
-header.flex.container.mx-auto.py-4.gap-4
-  aside.border.w-80
-    header
-      h2 Product Category
-    ul
+.flex.container.mx-auto.pt-4.gap-4
+  aside.border.w-80.flex.flex-col
+    header.bg-base-300.px-4.py-2
+      h2.text-xl.font-semibold Product Category
+    ul.menu
       li
         nuxt-link(to="/category/1") Product 1
       li
         nuxt-link(to="/category/2") Product 2
       li
         nuxt-link(to="/category/3") Product 3
-  .flex-1.border.h-full
+    nuxt-link.block.px-4.py-1.flex.justify-between.bg-base-200.border-t.mt-auto(
+      to="/category"
+    ) View all
+      i.bi.bi-chevron-right
+
+  .flex-1.border.h-60
+    img.w-full.h-full.block.object-cover(
+      src="https://evereditor.com/mui2.jpg"
+      alt="Muimui"
+    )
+.flex.container.mx-auto.pt-4.gap-4
+  .border.w-80
+    img.block(
+      src="https://evereditor.com/mui2.jpg"
+      alt="Muimui"
+    )
+  .flex-1.border.flex.flex-col
+    header.flex.px-4.py-2.border-b.items-center.justify-between
+      h2.text-lg.font-semibold Newest Products
+      nuxt-link.text-xs(to="/products") View all
+        i.bi.bi-chevron-right
+    .flex.flex-1
+      nuxt-link.flex-1.px-4.py-2.border-r.flex.flex-col.items-center
+        img.block.w-24(
+          src="https://evereditor.com/mui2.jpg"
+          alt="Muimui"
+        )
+        h3 Muimui
+        p.text-xs SiT2025B MEMS OscillatorsSiTime’s SiT2025B series offers board-level solder joint reliabi...
+      nuxt-link.flex-1.px-4.py-2.border-r.flex.flex-col.items-center
+        img.block.w-24(
+          src="https://evereditor.com/mui2.jpg"
+          alt="Muimui"
+        )
+        h3 Muimui
+        p.text-xs SiT2025B MEMS OscillatorsSiTime’s SiT2025B series offers board-level solder joint reliabi...
+      nuxt-link.flex-1.px-4.py-2.border-r.flex.flex-col.items-center
+        img.block.w-24(
+          src="https://evereditor.com/mui2.jpg"
+          alt="Muimui"
+        )
+        h3 Muimui
+        p.text-xs SiT2025B MEMS OscillatorsSiTime’s SiT2025B series offers board-level solder joint reliabi...
+      nuxt-link.flex-1.px-4.py-2.border-r.flex.flex-col.items-center
+        img.block.w-24(
+          src="https://evereditor.com/mui2.jpg"
+          alt="Muimui"
+        )
+        h3 Muimui
+        p.text-xs SiT2025B MEMS OscillatorsSiTime’s SiT2025B series offers board-level solder joint reliabi...
+.container.mx-auto.mt-4.border
+  header.flex.px-4.py-2.border-b.items-center.justify-between
+    h2.text-xl.font-semibold Hot Selling Products
+    nuxt-link.text-xs(to="/products") View all
+      i.bi.bi-chevron-right
+  p
+
 </template>
 
 <script lang="ts">
