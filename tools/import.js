334e5ef49641af147a734e6188024acc9986c1c1
Meathill <meathill@gmail.com>
2023-08-27 17:06:05 +0800

    bug: fixed import script and added logging

--- tools/import.js
+++ tools/import.js
@@ -9,21 +9,22 @@ import categories from './categories.js';
 
 async function createCategory(cate, parent) {
   const slug = slugify(cate, { lower: true });
-  const { rows } = await sql`INSERT INTO category
+  const { rows } = await sql`INSERT INTO "category"
 (name, slug, parent)
 VALUES (${cate}, ${slug}, ${parent})
 RETURNING id;`;
   return rows[0].id;
 }
 async function createProduct(productName, description, images, subCategoryId, productModel, specifications) {
-  const slug = slugify(productName, { lower: true });
-  console.log('Create product:', `INSERT INTO product
+  let slug = slugify(productName, { lower: true });
+  // check slug exists
+  const { rows: exists } = await sql`SELECT id FROM "product" WHERE slug = ${slug}`;
+  if (exists.length > 0) {
+    slug = slug + '-' + productModel;
+  }
+  const { rows } = await sql`INSERT INTO "product"
 (name, slug, description, images, category, model, more)
-VALUES ('${productName}', '${slug}', '${description}', '${JSON.stringify(images)}', ${subCategoryId}, '${productModel}', '${JSON.stringify(specifications)}')
-RETURNING id;`);
-  const { rows } = await sql`INSERT INTO product
-(name, slug, description, images, category, model, more)
-VALUES ('${productName}', '${slug}', '${description}', '${JSON.stringify(images)}', ${subCategoryId}, '${productModel}', '${JSON.stringify(specifications)}')
+VALUES (${productName}, ${slug}, ${description}, ${images}, ${subCategoryId}, ${productModel}, ${specifications})
 RETURNING id;`;
   return rows[0].id;
 }
@@ -43,20 +44,23 @@ const redis = new Redis(6379, '34.150.103.85', {
 let i = 0;
 
 for (const topType in categories) {
+  console.log('Top type:', topType);
   for (const secType in categories[topType]) {
+    console.log('Sec type:', secType);
     // 二级分类需要写入
-    let categoryId = created[secType];
+    let categoryId = created.category[secType];
     if (!categoryId) {
       categoryId = await createCategory(secType);
-      created[secType] = categoryId;
+      created.category[secType] = categoryId;
       await writeFile(storeFile, JSON.stringify(created));
     }
     for (const pType in categories[topType][secType]) {
+      console.log('Product type:', pType);
       // 三级分类需要写入
-      let subCategoryId = created[pType];
+      let subCategoryId = created.category[pType];
       if (!subCategoryId) {
         subCategoryId = await createCategory(pType, categoryId);
-        created[pType] = subCategoryId;
+        created.category[pType] = subCategoryId;
         await writeFile(storeFile, JSON.stringify(created));
       }
 
@@ -69,18 +73,23 @@ for (const topType in categories) {
 
       for (const k in allInfoList) {
         const info = JSON.parse(allInfoList[k]);
-        console.log('Fetch info:', info);
 
-        // 输出基本信息
         const productModel = info.model;
         const productName = info.name;
-        if (created[productModel]) {
+        if (created.product[productModel]) {
+          i++;
+          console.log('Product exists:', productModel, '; count:', i);
           continue;
         }
+        // 输出基本信息
+        console.log('Fetch info:', info);
 
         // 读取产品信息
         let data = await redis.get(info.model);
-        const specifications = JSON.parse(data);
+        let specifications = JSON.parse(data);
+        if (Array.isArray(specifications)) {
+          specifications = {};
+        }
         console.log('Specifications:', specifications);
 
         // 读取图片信息。先不下载图片，蹭他们的试试
@@ -95,7 +104,7 @@ for (const topType in categories) {
 
         // 将产品插入表
         const productId = await createProduct(productName, description, images, subCategoryId, productModel, specifications);
-        created[productModel] = productId;
+        created.product[productModel] = productId;
         await writeFile(storeFile, JSON.stringify(created));
 
         // 提取产品参数
@@ -104,20 +113,20 @@ for (const topType in categories) {
           for (const specKey in specGroup) {
             const specValue = specGroup[key];
             const storeKey = `spec_${subCategoryId}-${specKey}`;
-            let specId = created[storeKey];
+            let specId = created.spec[storeKey];
             if (!specId) {
-              const { rows } = await sql`INSERT INTO specification
+              const { rows } = await sql`INSERT INTO "specification"
 (name, type, category)
-VALUES ("${specKey}", 1, "${subCategoryId}")
+VALUES (${specKey}, 1, ${subCategoryId})
 RETURNING id;`;
               specId = rows[0].id;
-              created[storeKey] = specId;
+              created.spec[storeKey] = specId;
               await writeFile(storeFile, JSON.stringify(created));
             }
             // 插入参数记录
-            await sql`INSERT INTO productSpec
-(productId, specId, value)
-VALUES (${productId}, ${specId}, "${specValue}");`;
+            await sql`INSERT INTO "productSpec"
+("productId", "specId", value)
+VALUES (${productId}, ${specId}, ${specValue});`;
           }
         }
 
@@ -127,3 +136,6 @@ VALUES (${productId}, ${specId}, "${specValue}");`;
     }
   }
 }
+
+console.log('done');
+process.exit();
