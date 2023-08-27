ce0405868ab18ec047db3f427c4ba1e04b513480
Meathill <meathill@gmail.com>
2023-08-27 18:15:56 +0800

    feature: import data from target site (#1)
    * feature: import data from target site

---
+++ tools/categories.js
@@ -0,0 +1,76 @@
+export default {
+  'Motors & Drivers': {
+    'Stepper Motor': {
+      'Open Loop': 'https://www.omc-stepperonline.com/hybrid-stepper-motor',
+      'Closed Loop': 'https://www.omc-stepperonline.com/closed-loop',
+      'Geared Stepper': 'https://www.omc-stepperonline.com/geared-stepper-motor',
+      'Linear Stepper': 'https://www.omc-stepperonline.com/linear-motor',
+      'Stepper Motor Driver': 'https://www.omc-stepperonline.com/stepper-motor-driver',
+      'PM Stepper': 'https://www.omc-stepperonline.com/permanent-magnet-stepper-motor',
+      'Integrated Motor': 'https://www.omc-stepperonline.com/integrated-stepper-motor',
+      'Special Motor': 'https://www.omc-stepperonline.com/special',
+    },
+    'DC Motor': {
+      'BLDC Motor': 'https://www.omc-stepperonline.com/bldc-motor',
+      'DC Gear Motor': 'https://www.omc-stepperonline.com/dc-gear-motor',
+    },
+    'AC Motor': {
+      'AC Gear Motor': 'https://www.omc-stepperonline.com/ac-gear-motor',
+      'CNC Spindle Motor': 'https://www.omc-stepperonline.com/cnc-spindle-motor',
+    },
+    'Servo Motor': {
+      'AC Servo Motor': 'https://www.omc-stepperonline.com/ac-servo-motor',
+      'DC Servo Motor': 'https://www.omc-stepperonline.com/integrated-servo-motor',
+    },
+    'DIY Kit': {
+      'Robot Kit': 'https://www.omc-stepperonline.com/robot-kit-1',
+      '3D Printer Kit': 'https://www.omc-stepperonline.com/3d-printer-kit',
+    },
+  },
+  'Electrics & Controllers': {
+    'Power Supply': {
+      'Power Supply': 'https://www.omc-stepperonline.com/power-supply',
+    },
+    'Motor Encoder': {
+      'Rotary Encoder': 'https://www.omc-stepperonline.com/rotary-encoder',
+      'Optical Encoder': 'https://www.omc-stepperonline.com/optical-encoder',
+      'Magnetic Encoder': 'https://www.omc-stepperonline.com/encoder',
+    },
+    'Motor Brake': {
+      'Motor Brake': 'https://www.omc-stepperonline.com/stepper-motor-brake',
+    },
+    'Variable Frequency Drive': {
+      'Variable Frequency Drive': 'https://www.omc-stepperonline.com/variable-frequency-drive',
+    },
+    'Cable & Connector': {
+      'Stepper Motor Cable': 'https://www.omc-stepperonline.com/stepper-motor-cable-1',
+      'Servo Motor Cable': 'https://www.omc-stepperonline.com/servo-motor-cable',
+      'Communication Cable': 'https://www.omc-stepperonline.com/communication-cable',
+    },
+  },
+  'Mechanical & Accessories': {
+    Gearbox: {
+      'EG Series': 'https://www.omc-stepperonline.com/eg-series-planetary-gearbox',
+      'MG Series': 'https://www.omc-stepperonline.com/mg-series-planetary-gearbox',
+      'TG Series': 'https://www.omc-stepperonline.com/tg-series-planetary-gearbox',
+      'Right Angle': 'https://www.omc-stepperonline.com/right-angle-gearbox',
+      'Worm Gearbox': 'https://www.omc-stepperonline.com/worm-gearbox',
+      'Gearbox Accessories': 'https://www.omc-stepperonline.com/gearbox-accessories',
+    },
+    'Motor Bracket': {
+      'For Motor': 'https://www.omc-stepperonline.com/bracket-for-motor',
+      'For Gearbox': 'https://www.omc-stepperonline.com/bracket-for-gearbox',
+    },
+    'Shaft Coupling': {
+      'Single Disc': 'https://www.omc-stepperonline.com/flexible-single-disc-shaft-coupler',
+      'Double Disc': 'https://www.omc-stepperonline.com/flexible-double-disc-shaft-coupler',
+      'Flexible Jaw': 'https://www.omc-stepperonline.com/flexible-jaw-shaft-coupler',
+      Rigid: 'https://www.omc-stepperonline.com/rigid-shaft-coupler',
+      Flexible: 'https://www.omc-stepperonline.com/flexible-shaft-coupler',
+    },
+    'Vibration Damper': {
+      'Vibration Damper': 'https://www.omc-stepperonline.com/vibration-damper',
+    },
+  },
+
+};
